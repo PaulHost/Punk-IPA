e9cdc3310d850e31d33fda5857f90a941217bba9
paulhostev <pg@rention.com>
2017-11-17 13:04:10 +0200

    recyclerview pagination

--- app/src/main/java/ph/hostev/paul/punk_ipa/activities/MainActivity.java
+++ app/src/main/java/ph/hostev/paul/punk_ipa/activities/MainActivity.java
@@ -16,6 +16,7 @@ import ph.hostev.paul.punk_ipa.beans.Beer;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int pagimation = 1;
     List<Beer> beerList = new ArrayList<>();
     RecyclerView recyclerView;
     BeerAdapter beerAdapter;
@@ -30,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(beerAdapter);
-        App.getAPI().get(1, null, new Callback<List<Beer>>() {
+        recyclerView.addOnScrollListener(scrollListener());
+        load();
+    }
+
+    private void load(){
+        App.getAPI().get(pagimation, null, new Callback<List<Beer>>() {
             @Override
             public void onSuccess(final List<Beer> beers) {
                 recyclerView.post(new Runnable() {
@@ -38,9 +44,27 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         beerList.addAll(beers);
                         beerAdapter.notifyDataSetChanged();
+                        pagimation++;
                     }
                 });
             }
         });
+    }
+
+    RecyclerView.OnScrollListener scrollListener(){
+        return new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (layoutManager.getItemCount() - 5 == layoutManager.findLastVisibleItemPosition()){
+                    load();
+                }
+            }
+        };
     }
 }
