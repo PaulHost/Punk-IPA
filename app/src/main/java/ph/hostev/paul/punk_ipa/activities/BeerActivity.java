8efce43ea06f3c23ec0a218089df0c288eb371e4
paulhostev <pg@rention.com>
2017-11-18 01:13:51 +0200

    beer activity

--- app/src/main/java/ph/hostev/paul/punk_ipa/activities/BeerActivity.java
+++ app/src/main/java/ph/hostev/paul/punk_ipa/activities/BeerActivity.java
@@ -2,6 +2,10 @@ package ph.hostev.paul.punk_ipa.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.sql.SQLException;
 
@@ -22,5 +26,17 @@ public class BeerActivity extends AppCompatActivity {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        Picasso.with(this).load(beer.getImageUrl())
+                .into((ImageView) findViewById(R.id.beer_cover));
+
+        ((TextView) findViewById(R.id.name)).setText(beer.getName());
+        ((TextView) findViewById(R.id.tagline)).setText(beer.getTagline());
+        ((TextView) findViewById(R.id.first_brewed)).setText(beer.getFirstBrewed());
+        ((TextView) findViewById(R.id.abv)).setText(String.valueOf(beer.getAbv()));
+        ((TextView) findViewById(R.id.ibu)).setText(String.valueOf(beer.getAbv()));
+        ((TextView) findViewById(R.id.ebc)).setText(String.valueOf(beer.getEbc()));
+        ((TextView) findViewById(R.id.description)).setText(beer.getDescription());
+
     }
 }
