e09d075fa7c2f126938cda2d32d734de3985b73b
paulhostev <pg@rention.com>
2017-11-18 16:10:25 +0200

    little refactoring

--- app/src/main/java/ph/hostev/paul/punk_ipa/adapters/BeerAdapter.java
+++ app/src/main/java/ph/hostev/paul/punk_ipa/adapters/BeerAdapter.java
@@ -29,7 +29,7 @@ public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.beer_item_card_view, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.beer_item, parent, false);
         return new ViewHolder(v);
     }
 
