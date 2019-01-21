2b2e46737738f278e5d6440c2bcc00c7030eb350
paulhostev <pg@rention.com>
2019-01-21 23:17:05 +0300

    [rx_http] - Reactive HTTPsClient

--- app/src/main/java/ph/hostev/paul/punk_ipa/api/HTTPsClient.java
+++ app/src/main/java/ph/hostev/paul/punk_ipa/api/HTTPsClient.java
@@ -2,9 +2,11 @@ package ph.hostev.paul.punk_ipa.api;
 
 import java.util.List;
 
+import io.reactivex.Single;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.annotations.Nullable;
 import ph.hostev.paul.punk_ipa.beans.Beer;
 import ph.hostev.paul.punk_ipa.beans.SortParameters;
-import ph.hostev.paul.punk_ipa.parsing.BeerParser;
 
 import static ph.hostev.paul.punk_ipa.Constants.ABV_GT;
 import static ph.hostev.paul.punk_ipa.Constants.ABV_LT;
@@ -15,39 +17,23 @@ import static ph.hostev.paul.punk_ipa.Constants.IBU_GT;
 import static ph.hostev.paul.punk_ipa.Constants.IBU_LT;
 import static ph.hostev.paul.punk_ipa.Constants.PAGE;
 
-public class HTTPsClient {
+public class HTTPsClient extends HTTPs {
 
-    private HTTPs https;
-    private BeerParser parser;
+    @NonNull
+    public Single<List<Beer>> get(@Nullable Integer page, @Nullable SortParameters param) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BEERS).append(PAGE).append(page);
+        if (param != null) {
+            if (param.getAbv_gt() > 0) sb.append(ABV_GT).append(param.getAbv_gt());
+            if (param.getAbv_lt() > 0) sb.append(ABV_LT).append(param.getAbv_lt());
+            if (param.getEbc_gt() > 0) sb.append(EBC_GT).append(param.getEbc_gt());
+            if (param.getEbc_lt() > 0) sb.append(EBC_LT).append(param.getEbc_lt());
+            if (param.getIbu_gt() > 0) sb.append(IBU_GT).append(param.getIbu_gt());
+            if (param.getIbu_lt() > 0) sb.append(IBU_LT).append(param.getIbu_lt());
+        }
+        String url = sb.toString();
 
-    public HTTPsClient() {
-        https = new HTTPs();
-        parser = new BeerParser();
-    }
-
-    public void get(final int page, final SortParameters param, final Callback<List<Beer>> listCallback) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                StringBuilder sb = new StringBuilder();
-                sb.append(BEERS).append(PAGE).append(page);
-                if (param != null) {
-                    if (param.getAbv_gt() > 0) sb.append(ABV_GT).append(param.getAbv_gt());
-                    if (param.getAbv_lt() > 0) sb.append(ABV_LT).append(param.getAbv_lt());
-                    if (param.getEbc_gt() > 0) sb.append(EBC_GT).append(param.getEbc_gt());
-                    if (param.getEbc_lt() > 0) sb.append(EBC_LT).append(param.getEbc_lt());
-                    if (param.getIbu_gt() > 0) sb.append(IBU_GT).append(param.getIbu_gt());
-                    if (param.getIbu_lt() > 0) sb.append(IBU_LT).append(param.getIbu_lt());
-                }
-                String url = sb.toString();
-
-                https.get(url, new Callback<String>() {
-                    @Override
-                    public void onSuccess(String s) {
-                        listCallback.onSuccess(parser.beers(s));
-                    }
-                });
-            }
-        }).start();
+        return this.get(url)
+                   .flatMap(this::beers);
     }
 }
